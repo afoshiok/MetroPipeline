09859992b3e9f861d13381b67ba185b5689ce9d1
Favour O. <ayokunnuo56@gmail.com>
2026-04-21 14:52:19 -0500

    Fixed vehicles asset

--- pipeline/src/pipeline/defs/assets.py
+++ pipeline/src/pipeline/defs/assets.py
@@ -1,6 +1,7 @@
 import dagster as dg
 from dotenv import load_dotenv
 import os
+import pandas as pd
 
 load_dotenv()
 
@@ -8,5 +9,6 @@ METRO_API = os.getenv('METRO_API_KEY')
 
 @dg.asset()
 def vehicles_dataset() -> str:
-    return f"https://api.ridemetro.org/data/Vehicles?subscription-key={METRO_API}"
-
+    vehicles_url = f"https://api.ridemetro.org/data/Vehicles?subscription-key={METRO_API}"
+    vehicles_df = pd.read_json(vehicles_url)
+    return f"Total vehicles: {len(vehicles_df)}"
